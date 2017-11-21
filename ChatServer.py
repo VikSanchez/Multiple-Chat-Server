3c4ecf67a8911e9ecec056870fdc06b66eb4de5a
VikasDanny <vikasdanny56@gmail.com>
2017-11-21 18:31:39 +0000

    Update ChatServer.py
    imported libraries and formatted strings.

--- ChatServer.py
+++ ChatServer.py
@@ -9,16 +9,29 @@ Created on Sat Oct  7 16:45:17 2017
 # chat_server.py
  
 import sys, socket, select
+from Queue import Queue
+from threading import Thread
+from threading import Lock  
+import re as regex  
+import ChatRoom 
+import hashlib  
+import os 
 
-def check_msg(msg):
-	print('Checking for your connection')
-	if (msg.find('JOIN_CHATROOM'.encode('utf-8'))+1):
-		return(1)	
-	elif (msg.find('LEAVE_CHATROOM'.encode('utf-8'))+1):
-		return(2)
-	elif (msg.find('DISCONNECT'.encode('utf-8'))+1):
-		return(3)
-	elif (msg.find('CHAT:'.encode('utf-8'))+1):
-		return(4)	
-	else:
-		return(5)
+port = 0
+host = sys.argv[0]
+
+# All client stored in the queue
+clients = Queue()
+
+# This array will contain all the details of chat rooms
+chat_rooms_array = {}
+
+# it would create synchronizing Chat room creation and chats
+rooms_lock = Lock()
+
+# All the valid input messages to the server from the client
+valid_join_msg = r"JOIN_CHATROOM: ?(.*)(?:\s|\\n)CLIENT_IP: ?(.*)(?:\s|\\n)PORT: ?(.*)(?:\s|\\n)CLIENT_NAME: ?(.*)"
+valid_hello_msg = r"HELO ?(.*)\s"
+valid_leave_msg = r"LEAVE_CHATROOM: ?(.*)(?:\s|\\n)JOIN_ID: ?(.*)(?:\s|\\n)CLIENT_NAME: ?(.*)"
+valid_chat_msg = r"CHAT: ?(.*)(?:\s|\\n)JOIN_ID: ?(.*)(?:\s|\\n)CLIENT_NAME: ?(.*)(?:\s|\\n)MESSAGE: ?(.*)"
+valid_disconnect_msg = r"DISCONNECT: ?(.*)(?:\s|\\n)PORT: ?(.*)(?:\s|\\n)CLIENT_NAME: ?(.*)"
